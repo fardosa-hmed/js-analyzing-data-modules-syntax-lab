2caf2687c828ed33664ab27b93c6430a4815cffd
Fardosa Ahmed <shzeynab82@gmail.com>
2026-04-19 10:09:06 +0300

    updated js file

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+require('datejs');
 
+function combineUsers(...args) {
+    const combinedObject = {
+        users: []
+    };
+
+    for (let i = 0; i < args.length; i++) {
+        combinedObject.users = [...combinedObject.users, ...args[i]];
+    }
+
+    combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+    return combinedObject;
+}
+
+module.exports = combineUsers;
 
 
 module.exports = {
